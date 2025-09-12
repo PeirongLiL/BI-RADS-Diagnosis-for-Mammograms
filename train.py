8490c7044bbc258b90557f7f928bfa307968b017
fjnu_mia <75237078+PeirongLiL@users.noreply.github.com>
2025-09-12 11:29:22 +0800

    train.py

--- train.py
+++ train.py
@@ -224,17 +224,6 @@ class Logger(object):
         pass
 
 if __name__ == '__main__':
-    #最全的
-    seed=3407
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)  # 保证随机结果可复现
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-
-    print(seed)
 
 
     parser = argparse.ArgumentParser()
@@ -262,3 +251,4 @@ if __name__ == '__main__':
     for i in range(10):
         print(i)
         main(opt,i)
+
